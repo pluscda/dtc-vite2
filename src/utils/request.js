ac8f51d259ba3e867bd314f365079b162879d0e0
alex <pluscda@gmail.com>
2021-03-26 14:29:53 +0800

    g

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,82 +1,31 @@
+//https://www.mdeditor.tw/pl/gc7t/zh-hk
 import axios from "axios";
-import { Message, Msgbox } from "element3";
-import store from "/@/store";
-
-// 创建axios实例
 const service = axios.create({
-  // 在请求地址前面加上baseURL
-  baseURL: import.meta.env.VITE_BASE_API,
-  // 当发送跨域请求时携带cookie
-  // withCredentials: true,
-  timeout: 5000,
+  baseURL: "/",
+  timeout: 720 * 1000, // Timeout
 });
-
-// 请求拦截
+// 發起請求之前的攔截器
 service.interceptors.request.use(
-  (config) => {
-    // 指定请求令牌
-    // if (store.getters.token) {
-    // // 自定义令牌的字段名为X-Token，根据咱们后台再做修改
-    // config.headers["X-Token"] = store.getters.token;
-    // }
-    config.headers["X-Token"] = "my token";
+  config => {
+    // 如果有token 就攜帶tokon
+    config.headers.Authorization = "Bearer " + sessionStorage.token;
     return config;
   },
-  (error) => {
-    // 请求错误的统一处理
-    console.log(error); // for debug
-    return Promise.reject(error);
-  }
+  error => Promise.reject(error)
 );
-
-// 响应拦截器
+// 響應攔截器
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-   */
-
-  /**
-   * 通过判断状态码统一处理响应，根据情况修改
-   * 同时也可以通过HTTP状态码判断请求结果
-   */
-  (response) => {
+  response => {
     const res = response.data;
-
-    // 如果状态码不是20000则认为有错误
-    if (res.code !== 20000) {
-      Message.error({
-        message: res.message || "Error",
-        duration: 5 * 1000,
-      });
-
-      // 50008: 非法令牌; 50012: 其他客户端已登入; 50014: 令牌过期;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // 重新登录
-        Msgbox.confirm("您已登出, 请重新登录", "确认", {
-          confirmButtonText: "重新登录",
-          cancelButtonText: "取消",
-          type: "warning",
-        }).then(() => {
-          store.dispatch("user/resetToken").then(() => {
-            location.reload();
-          });
-        });
-      }
-      return Promise.reject(new Error(res.message || "Error"));
+    if (response.status !== 200) {
+       const ret = response?.data ? response.data : '' + "error Ajax";
+       return Promise.reject(ret);
     } else {
       return res;
     }
   },
-  (error) => {
-    console.log("err" + error); // for debug
-    Message({
-      message: error.message,
-      type: "error",
-      duration: 5 * 1000,
-    });
+  error => {
     return Promise.reject(error);
   }
 );
-
 export default service;
