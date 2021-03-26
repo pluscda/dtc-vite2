193eec3f509ec9700254826b48be673051e2a324
alex <pluscda@gmail.com>
2021-03-26 10:43:31 +0800

    g

--- vite.config.js
+++ vite.config.js
@@ -30,10 +30,6 @@ export default {
     ViteIcons(),
     ,viteMockServe({ supportTs: false }), 
     vueI18n({
-    // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-    // compositionOnly: false,
-
-    // you need to set i18n resource including paths !
     include: path.resolve(__dirname, './src/locales/**')
   })],
 };
