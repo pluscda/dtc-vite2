bcde94e13a55660dc5f4bbbb8ff7eaf9de8ba8a3
alex <pluscda@gmail.com>
2021-03-29 09:13:23 +0800

    g

--- src/main.js
+++ src/main.js
@@ -1,27 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
-
-
 //tailwind css
 import "styles/tailwind.css"
-
 // 全局样式
 import "styles/index.scss";
-
-
 // element3
 import element3 from "plugins/element3";
-
 // router
 import router from "/@/router";
-
 // store
 import store from "/@/store";
-
 //partical js
 import Particles from "particles.vue3";
-
 
 // i18n
 import { createI18n } from "vue-i18n";
