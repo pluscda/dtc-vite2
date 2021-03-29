e4536d87d3e54f56462a9c1ee107765252ef400a
alex <pluscda@gmail.com>
2021-03-29 11:52:37 +0800

    g

--- src/views/users/list.vue
+++ src/views/users/list.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="app-container">
     <header class="dtc-page-header">役男清冊查詢</header>
-    <header class="dtc-grid-header dtc-headers dtc-template-columns">
+    <div class="dtc-search-filters ml-1">
+      <el-input placeholder="搜尋身份證字號" v-model="input1">
+        <template #prepend>身份證號</template>
+      </el-input>
+      <el-input placeholder="請輸入檢查狀態" v-model="input1">
+        <template #prepend>檢查狀態</template>
+      </el-input>
+      <el-input placeholder="請輸入體檢時間" v-model="input1">
+        <template #prepend>體檢時間</template>
+      </el-input>
+    </div>
+    <header class="dtc-grid-header dtc-headers dtc-template-columns ml-1">
       <div>操作</div>
       <div v-for="(item, i) in headers" :key="i" @click="sort(item)">
         {{ item.name }}
@@ -17,7 +28,7 @@
       </div>
     </header>
     <main
-      class="dtc-grid-header dtc-grid-body dtc-template-columns text-black"
+      class="dtc-grid-header dtc-grid-body dtc-template-columns text-black ml-1"
       v-for="(item, i) in list"
       :key="i"
       :style="i % 2 == 0 ? 'background-color: #F5F5F5;' : 'background-color: #E0E0E0;'"
@@ -30,7 +41,7 @@
       <div>{{ item.Seq || "暫無資料" }}</div>
     </main>
 
-    <!-- 分页 -->
+    <!-- 分頁 -->
     <pagination v-show="total > 0" :total="total" v-model:page="listQuery.page" v-model:limit="listQuery.limit" @pagination="getList"></pagination>
   </div>
 </template>
@@ -45,7 +56,7 @@ import { useList } from "./model/userModel";
 let headers = [
   { name: "ID", key: "id", sortDesc: null },
   { name: "建立者", key: "name", sortDesc: null },
-  { name: "年龄", key: "age", sortDesc: null },
+  { name: "年齡", key: "age", sortDesc: null },
 ];
 
 export default {
@@ -54,12 +65,12 @@ export default {
     Pagination,
   },
   setup() {
-    // 玩家列表数据
+    // 玩家列表數據
     const router = useRouter();
     headers = ref(headers);
     const { state, getList, delItem } = useList();
 
-    // 用户更新
+    // 用戶更新
     function handleEdit({ row }) {
       router.push({
         name: "userEdit",
@@ -67,12 +78,12 @@ export default {
       });
     }
 
-    // 删除玩家
+    // 刪除玩家
     function handleDelete({ row }) {
       delItem(row.id).then(() => {
-        // todo:删除这一行，或者重新获取数据
-        // 通知用户
-        Message.success("删除成功！");
+        // todo:刪除這一行，或者重新獲取數據
+        // 通知用戶
+        Message.success("刪除成功！");
       });
     }
 
