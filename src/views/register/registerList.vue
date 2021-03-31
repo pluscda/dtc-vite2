a3c03f8a159737d26232dbb9af76c9e47654f431
alex <pluscda@gmail.com>
2021-03-31 16:52:18 +0800

    g

--- src/views/register/registerList.vue
+++ src/views/register/registerList.vue
@@ -5,10 +5,10 @@
     </header>
     <nav class="mb-2 ml-1 dtc-search-filters">
       <DtxInputGroup prepend="掛號日期">
-        <Calendar class="h-10" v-model="value" placeholder="輸入日期" :showIcon="true" />
+        <Calendar class="h-10" v-model="value" placeholder="輸入日期" :showIcon="true" dateFormat="yy-mm-dd" />
       </DtxInputGroup>
       <div class="mx-1 pt-2">至</div>
-      <Calendar class="h-10" v-model="value" placeholder="輸入日期" :showIcon="true" />
+      <Calendar class="h-10" v-model="value" placeholder="輸入日期" :showIcon="true" dateFormat="yy-mm-dd" />
       <DtxInputGroup prepend="身分證號">
         <el-input placeholder="搜尋身份證字號" v-model="input2" />
       </DtxInputGroup>
