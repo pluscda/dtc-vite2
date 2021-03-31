f07795c48cf518d7f7075be10f001d420bfb324e
alex <pluscda@gmail.com>
2021-03-31 11:08:35 +0800

    g

--- src/views/users/list.vue
+++ src/views/users/list.vue
@@ -69,7 +69,7 @@
       <DtxInputGroup prepend="健保卡序">
         <el-input placeholder="搜尋檢查狀態" v-model="input1" />
       </DtxInputGroup>
-      <DtxInputGroup prepend="掛號取卡">
+      <DtxInputGroup prepend="掛號取卡" class="h-10">
         <van-switch v-model="takeCard" size="small" class="ml-2 h-9 mt-3" />
         <span class="ml-2 mt-2 text-sm text-gray-600" @click="takeCard = !takeCard">掛號後取卡</span>
         <!-- <el-checkbox class="ml-2 h-9 mt-2" v-model="checked">掛號後取卡</el-checkbox> -->
