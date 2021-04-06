79018848cd602aac0cedbd6cb69007003c450d65
Ray <justdoit1990@datacom.com.tw>
2021-04-06 11:58:44 +0800

    navbar-avatar

--- src/layouts/components/Navbar.vue
+++ src/layouts/components/Navbar.vue
@@ -1,18 +1,33 @@
 <template>
-  <nav class="text-blue-200 bg-gradient-to-r from-blue-900 to-blue-700 dtc-nav-bar">
+  <nav
+    class="text-blue-200 bg-gradient-to-r from-blue-900 to-blue-700 dtc-nav-bar"
+  >
     <i-uim:layer-group class="pt-2 text-5xl"></i-uim:layer-group>
-    <div class="pt-5 text-xl transform -translate-x-4 cursor-pointer" @click="$router.push('/home')">基本醫療服務</div>
+    <div
+      class="pt-5 text-xl transform -translate-x-4 cursor-pointer"
+      @click="$router.push('/home')"
+    >
+      基本醫療服務
+    </div>
     <section class="flex gap-8 cursor-pointer">
       <div @click="$router.push('/home')">首頁</div>
       <el-dropdown class="max-h-10" :show-timeout="1">
-        <span class="text-blue-200">門急診掛號<i class="el-icon-arrow-down el-icon--right"></i> </span>
+        <span class="text-blue-200"
+          >門急診掛號<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
         <el-dropdown-menu>
-          <el-dropdown-item @click="$router.replace('/users/list')">掛號</el-dropdown-item>
-          <el-dropdown-item @click="$router.replace('/register')">掛號總覽清單</el-dropdown-item>
+          <el-dropdown-item @click="$router.replace('/users/list')"
+            >掛號</el-dropdown-item
+          >
+          <el-dropdown-item @click="$router.replace('/register')"
+            >掛號總覽清單</el-dropdown-item
+          >
         </el-dropdown-menu>
       </el-dropdown>
       <el-dropdown class="max-h-10" :show-timeout="1">
-        <span class="text-blue-200">線上諮詢<i class="el-icon-arrow-down el-icon--right"></i> </span>
+        <span class="text-blue-200"
+          >線上諮詢<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
         <el-dropdown-menu>
           <el-dropdown-item>諮詢-1</el-dropdown-item>
           <el-dropdown-item>諮詢-2</el-dropdown-item>
@@ -21,8 +36,11 @@
       <div>歷史報告</div>
       <div>所得明細</div>
     </section>
-    <div class="grid grid-cols-3 cursor-pointer" @click="toggleBg">
-      <div class="mt-1"><i class="pi pi-cog" style="font-size: 18px"></i> <span class="text-blue-200 inline-block -mt-7">背景顏色</span></div>
+    <div class="grid grid-cols-2 cursor-pointer" @click="toggleBg">
+      <div class="mt-1">
+        <i class="pi pi-cog" style="font-size: 18px"></i>
+        <span class="text-blue-200 inline-block -mt-7">背景顏色</span>
+      </div>
       <OverlayPanel ref="bgPanel" showCloseIcon="true" :dismissable="true">
         <ul class="grid grid-cols-2 gap-8">
           <div class="p-field-radiobutton">
@@ -44,18 +62,45 @@
         </ul>
       </OverlayPanel>
     </div>
+    <div
+      class="grid grid-cols-1 cursor-pointer"
+      @click="toggleAvatar"
+      style="margin-right: 18px"
+    >
+      <div class="mt-1">
+        <i-carbon:user-avatar-filled-alt
+          style="font-size: 24px; margin-top: 22px"
+        ></i-carbon:user-avatar-filled-alt>
+      </div>
+      <OverlayPanel ref="avatarPanel" showCloseIcon="true" :dismissable="true">
+        <ul class="grid grid-cols-2 gap-2">
+          <div class="p-field-radiobutton" style="margin: 0 auto">
+            <Button label="登出" class="p-button" style="font-size: 0.9rem" />
+          </div>
+          <div class="p-field-radiobutton">
+            <Button
+              label="更改密碼"
+              class="p-button p-button-warning px-10"
+              style="font-size: 0.9rem"
+            />
+          </div>
+        </ul>
+      </OverlayPanel>
+    </div>
   </nav>
 </template>
 
 <script setup>
 import { ref, inject, computed, onMounted } from "vue";
 const bgPanel = ref(null);
+const avatarPanel = ref(null);
 const bgColor = ref("Taipei");
 const toggleBg = (event) => bgPanel.value.toggle(event);
+const toggleAvatar = (event) => avatarPanel.value.toggle(event);
 const global = inject("global");
 const axios = inject("axios");
 //const userColor = computed(() => global.userDefaultBgColor);
-//alert(global.userDefaultBgColor);
+// alert(global.userDefaultBgColor);
 // onMounted(() => {
 //   axios.get("/getUsers");
 // });
@@ -69,6 +114,6 @@ const axios = inject("axios");
   padding: 0 1rem;
   line-height: 70px;
   font-size: 1rem;
-  grid-template-columns: max-content 160px 1fr max-content;
+  grid-template-columns: max-content 160px 1fr max-content max-content;
 }
 </style>
