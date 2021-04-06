804d40daaa85b1a31344b177a057647af3181d36
alex <pluscda@gmail.com>
2021-04-06 13:51:22 +0800

    g

--- src/layouts/components/Navbar.vue
+++ src/layouts/components/Navbar.vue
@@ -1,33 +1,18 @@
 <template>
-  <nav
-    class="text-blue-200 bg-gradient-to-r from-blue-900 to-blue-700 dtc-nav-bar"
-  >
+  <nav class="text-blue-200 bg-gradient-to-r from-blue-900 to-blue-700 dtc-nav-bar">
     <i-uim:layer-group class="pt-2 text-5xl"></i-uim:layer-group>
-    <div
-      class="pt-5 text-xl transform -translate-x-4 cursor-pointer"
-      @click="$router.push('/home')"
-    >
-      基本醫療服務
-    </div>
+    <div class="pt-5 text-xl transform -translate-x-4 cursor-pointer" @click="$router.push('/home')">基本醫療服務</div>
     <section class="flex gap-8 cursor-pointer">
       <div @click="$router.push('/home')">首頁</div>
       <el-dropdown class="max-h-10" :show-timeout="1">
-        <span class="text-blue-200"
-          >門急診掛號<i class="el-icon-arrow-down el-icon--right"></i>
-        </span>
+        <span class="text-blue-200">門急診掛號<i class="el-icon-arrow-down el-icon--right"></i> </span>
         <el-dropdown-menu>
-          <el-dropdown-item @click="$router.push('/users/list')"
-            >掛號</el-dropdown-item
-          >
-          <el-dropdown-item @click="$router.push('/register')"
-            >掛號總覽清單</el-dropdown-item
-          >
+          <el-dropdown-item @click="$router.push('/users/list')">掛號</el-dropdown-item>
+          <el-dropdown-item @click="$router.push('/register')">掛號總覽清單</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
       <el-dropdown class="max-h-10" :show-timeout="1">
-        <span class="text-blue-200"
-          >線上諮詢<i class="el-icon-arrow-down el-icon--right"></i>
-        </span>
+        <span class="text-blue-200">線上諮詢<i class="el-icon-arrow-down el-icon--right"></i> </span>
         <el-dropdown-menu>
           <el-dropdown-item>諮詢-1</el-dropdown-item>
           <el-dropdown-item>諮詢-2</el-dropdown-item>
@@ -37,12 +22,12 @@
       <div>所得明細</div>
     </section>
     <div class="grid grid-cols-2 cursor-pointer" @click="toggleBg">
-      <div class="mt-1">
-        <i class="pi pi-cog" style="font-size: 18px"></i>
-        <span class="text-blue-200 inline-block -mt-7">背景顏色</span>
+      <div class="mt-2.5">
+        <i class="pi pi-cog" style="font-size: 24px"></i>
       </div>
-      <OverlayPanel ref="bgPanel" showCloseIcon="true" :dismissable="true">
-        <ul class="grid grid-cols-2 gap-8">
+      <OverlayPanel ref="bgPanel" showCloseIcon="true" class="relative" :dismissable="true">
+        <header class="bg-violet-600 text-center text-lg pt-2 absolute inset-x-0 inset-y-0 h-10 text-white">主題顏色</header>
+        <ul class="grid grid-cols-2 gap-8 mt-8">
           <div class="p-field-radiobutton">
             <RadioButton id="bgColor1" value="New York" v-model="bgColor" />
             <label for="bgColor1" class="pl-2">New York</label>
@@ -62,27 +47,18 @@
         </ul>
       </OverlayPanel>
     </div>
-    <div
-      class="grid grid-cols-1 cursor-pointer"
-      @click="toggleAvatar"
-      style="margin-right: 18px"
-    >
+    <div class="grid grid-cols-1 cursor-pointer" @click="toggleAvatar" style="margin-right: 18px">
       <div class="mt-1">
-        <i-carbon:user-avatar-filled-alt
-          style="font-size: 24px; margin-top: 22px"
-        ></i-carbon:user-avatar-filled-alt>
+        <i-carbon:user-avatar-filled-alt style="font-size: 24px; margin-top: 22px"></i-carbon:user-avatar-filled-alt>
       </div>
-      <OverlayPanel ref="avatarPanel" showCloseIcon="true" :dismissable="true">
-        <ul class="grid grid-cols-2 gap-2">
+      <OverlayPanel ref="avatarPanel" showCloseIcon="true" :dismissable="true" class="relative z-1">
+        <header class="bg-violet-600 text-center text-lg pt-2 absolute inset-x-0 inset-y-0 h-10 text-white z-0">個人中心</header>
+        <ul class="grid grid-cols-2 gap-2 pt-10 relative">
           <div class="p-field-radiobutton" style="margin: 0 auto">
             <Button label="登出" class="p-button" style="font-size: 0.9rem" />
           </div>
           <div class="p-field-radiobutton">
-            <Button
-              label="更改密碼"
-              class="p-button p-button-warning px-10"
-              style="font-size: 0.9rem"
-            />
+            <Button label="更改密碼" class="p-button p-button-warning px-10" style="font-size: 0.9rem" />
           </div>
         </ul>
       </OverlayPanel>
