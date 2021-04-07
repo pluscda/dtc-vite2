1c577bebbc40c1da89e30a0a7998e2d9670190fa
alex <pluscda@gmail.com>
2021-04-07 11:04:26 +0800

    g

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -3,11 +3,15 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { defineProps, ref, toRefs } from "vue";
 import { timer } from "rxjs";
 import { tryOnUnmounted } from "@vueuse/core";
 
-const timestamp = ref(60);
+const props = defineProps({
+  secs: Number,
+});
+
+const timestamp = ref(props.secs);
 const converToCurrentTime = (i) => (timestamp.value -= i);
 
 const sub = timer(0, 1000).subscribe(converToCurrentTime);
