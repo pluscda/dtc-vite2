77cf98b97f2918f06161546a8d4c195d8ec934bb
alex <pluscda@gmail.com>
2021-04-07 17:23:57 +0800

    g

--- src/views/DtcLogin.vue
+++ src/views/DtcLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="dtc-login">
     <section class="login-panel">
-      <h3 class="text-2xl text-white mb-11">掛號門診登入</h3>
+      <h3 class="text-2xl text-white mb-11">基本醫療服務登入</h3>
       <el-input placeholder="使用者名稱" v-model="input1" class="">
         <template #prepend>
           <i-ri:user-shared-fill />
