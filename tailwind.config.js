722551135a1426ef3fc4d29b0b18a154beb94930
alex <pluscda@gmail.com>
2021-04-08 09:08:30 +0800

    g

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
       backgroundImage: theme => ({ //bg-light-pattern  , bg-footer-texture
          'light-pattern': "linear-gradient(180deg, #E2E5FD 0%, #E8EAFF 47%, #C0C7F5 100%)",
          'light-pattern-active': "linear-gradient(180deg, #CED5FF 0%, #ABB8FF 100%)",
+         'dark-pattern': 'linear-gradient(rgb(33, 35, 37) 0%, rgb(30, 33, 34) 47%, rgb(45, 48, 50) 100%);',
          'footer-texture': "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(168,15,213,1) 35%, rgba(0,212,255,1) 100%)",
       })
      
