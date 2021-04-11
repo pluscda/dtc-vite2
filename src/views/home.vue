c13be2bc359b4e36f1e45127a5da08ccd44b43bd
Alex <pluscda@gmail.com>
2021-04-11 17:49:25 +0800

    your commit message

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="grid place-items-center">
-    <h1 class="text-2xl mt-12">HOME under construction</h1>
+    <h1 class="mt-12 text-2xl" @click="sub.next(111)">HOME under construction {{ test }}</h1>
   </div>
 </template>
 
+<script setup>
+import { Subject, pipe } from 'rxjs';
+import { mapTo, throttleTime, tap, switchMap } from 'rxjs/operators';
+const test = 'test';
+const sub = new Subject();
+
+const t = pipe(
+  throttleTime(4000),
+  tap(() => console.log('click'))
+);
+const getData = async () => {
+  return await fetch('https://jsonplaceholder.typicode.com/todos/1').then((s) => s.json());
+};
+sub.pipe(t, switchMap(getData)).subscribe(console.log);
+</script>
 <script>
 export default {
   data() {
     return {
-      value: "",
+      value: '',
     };
   },
 };
