66b1d36dbf5e7ad585cb03e0b497fcc6d120d982
Alex <pluscda@gmail.com>
2021-04-12 19:17:23 +0800

    your commit message

---
+++ src/layouts/indexWithSidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="overflow-x-hidden index-position">
+    <NavBar></NavBar>
+    <main class="grid overflow-x-hidden overflow-y-hidden my-main-part">
+      <aside></aside>
+      <router-view></router-view>
+    </main>
+    <el-backtop target=".index-position" :visibility-height="100" style="color: rgb(58 142 229)">
+      <i-mdi:arrow-up style="font-size: 24px"></i-mdi:arrow-up>
+    </el-backtop>
+  </section>
+</template>
+
+<script setup>
+import NavBar from '/@/layouts/components/Navbar.vue';
+</script>
+
+<style lang="scss" scoped>
+.index-position {
+  height: 100vh;
+  //overflow-x: auto;
+  overflow-y: auto;
+}
+
+aside {
+  background: var(--primay) !important;
+}
+
+.my-main-part {
+  grid-template-columns: 200px 1fr;
+}
+</style>
