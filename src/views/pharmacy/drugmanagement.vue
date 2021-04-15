9908375240255d4737ccd6cbe028482e6748e893
alex <pluscda@gmail.com>
2021-04-15 14:46:50 +0800

    g

--- src/views/pharmacy/drugmanagement.vue
+++ src/views/pharmacy/drugmanagement.vue
@@ -3,10 +3,7 @@
     <header class="dtc-page-header grid dtc-page-header__grid pr-2">
       <div>藥品資料維護</div>
     </header>
-    <nav
-      class="ml-1 dtc-search-filters mt-4"
-      style="margin-bottom: 1.5rem !important"
-    >
+    <nav class="ml-1 dtc-search-filters mt-4" style="margin-bottom: 1.5rem !important">
       <DtxInputGroup prepend="藥品編號">
         <el-input placeholder="搜尋藥品編號" v-model="searchDrugId" />
       </DtxInputGroup>
@@ -17,10 +14,7 @@
       <Button label="清除查詢" class="p-button-secondary" icon="pi pi-undo" />
     </nav>
 
-    <header
-      data-msg="註1:限低於庫存下可轉採購單"
-      class="my-title relative dtc-grid-grumanagement-header dtc-grid-header dtc-grid-header__divs dtc-template-columns mx-1"
-    >
+    <header data-msg="註1:限低於庫存下可轉採購單" class="my-title relative dtc-grid-grumanagement-header dtc-grid-header dtc-grid-header__divs dtc-template-columns mx-1">
       <div title="" class="title-word1">採購單(註1)</div>
       <div>操作</div>
       <div v-for="(item, i) in headers" :key="i" @click="sort(item)">
@@ -40,44 +34,31 @@
       class="dtc-grid-header dtc-grid-body dtc-template-columns text-black ml-1 mx-1"
       v-for="(item, i) in list"
       :key="i"
-      :style="
-        i % 2 == 0 ? 'background-color: #F5F5F5;' : 'background-color: #E0E0E0;'
-      "
+      :style="i % 2 == 0 ? 'background-color: #F5F5F5;' : 'background-color: #E0E0E0;'"
     >
       <div>
-        <InputSwitch
-          class="transform translate-y-1.5"
-          v-model="item.review"
-          size="small"
-          @click.stop="toggleDetail(item)"
-        ></InputSwitch>
+        <InputSwitch class="transform translate-y-1.5" v-model="item.review" size="small" @click.stop="toggleDetail(item)"></InputSwitch>
       </div>
       <div class="flex flex-none space-x-2">
         <Button label="編輯" class="p-button-sm" />
         <Button label="刪除" class="p-button-sm p-button-warning" />
       </div>
 
-      <div>{{ item.name || "暫無資料" }}</div>
-      <div>{{ item.age || "暫無資料" }}</div>
-      <div>{{ item.id || "暫無資料" }}</div>
-      <div>{{ item.name || "暫無資料" }}</div>
-      <div>{{ item.age || "暫無資料" }}</div>
-      <div>{{ item.id || "暫無資料" }}</div>
-      <div>{{ item.name || "暫無資料" }}</div>
-      <div>{{ item.age || "暫無資料" }}</div>
-      <div>{{ item.id || "暫無資料" }}</div>
-      <div>{{ item.name || "暫無資料" }}</div>
-      <div>{{ item.age || "暫無資料" }}</div>
-      <div>{{ item.id || "暫無資料" }}</div>
+      <div>{{ item.drugId || "暫無資料" }}</div>
+      <div>{{ item.drugName || "暫無資料" }}</div>
+      <div>{{ item.drugUnit || "暫無資料" }}</div>
+      <div>{{ item.drugStandard || "暫無資料" }}</div>
+      <div>{{ item.buyPrice || "暫無資料" }}</div>
+      <div>{{ item.sellPrice || "暫無資料" }}</div>
+      <div>{{ item.unknow || "暫無資料" }}</div>
+      <div>{{ item.stockMax || "暫無資料" }}</div>
+      <div>{{ item.stockMin || "暫無資料" }}</div>
+      <div>{{ item.countryFrom || "暫無資料" }}</div>
+      <div>{{ item.drugDose || "暫無資料" }}</div>
+      <div>{{ item.unknow || "暫無資料" }}</div>
     </main>
     <!-- 分頁 -->
-    <pagination
-      v-show="total > 0"
-      :total="total"
-      v-model:page="listQuery.page"
-      v-model:limit="listQuery.limit"
-      @pagination="getList"
-    ></pagination>
+    <pagination v-show="total > 0" :total="total" v-model:page="listQuery.page" v-model:limit="listQuery.limit" @pagination="getList"></pagination>
   </section>
 </template>
 
@@ -88,18 +69,18 @@ import { useList } from "../users/model/userModel";
 
 //身分證號
 let headers = [
-  { name: "藥品編號", key: "name", sortDesc: null },
-  { name: "藥品名稱", key: "name", sortDesc: null },
-  { name: "單位", key: "age", sortDesc: null },
-  { name: "規格", key: "age", sortDesc: null },
-  { name: "進價", key: "age", sortDesc: null },
-  { name: "售價", key: "age", sortDesc: null },
-  { name: "存量", key: "age", sortDesc: null },
-  { name: "庫存上限", key: "age", sortDesc: null },
-  { name: "庫存下限", key: "age", sortDesc: null },
-  { name: "產地", key: "age", sortDesc: null },
-  { name: "劑型", key: "age", sortDesc: null },
-  { name: "收費項目", key: "age", sortDesc: null },
+  { name: "藥品編號", key: "drugId", sortDesc: null },
+  { name: "藥品名稱", key: "drugName", sortDesc: null },
+  { name: "單位", key: "drugUnit", sortDesc: null },
+  { name: "規格", key: "drugStandard", sortDesc: null },
+  { name: "進價", key: "buyPrice", sortDesc: null },
+  { name: "售價", key: "sellPrice", sortDesc: null },
+  { name: "存量", key: "unknown", sortDesc: null },
+  { name: "庫存上限", key: "stockMax", sortDesc: null },
+  { name: "庫存下限", key: "stockMin", sortDesc: null },
+  { name: "產地", key: "countryFrom", sortDesc: null },
+  { name: "劑量", key: "drugDose", sortDesc: null },
+  { name: "收費項目", key: "unknown", sortDesc: null },
 ];
 
 export default {
@@ -151,10 +132,7 @@ export default {
 .dtc-template-columns {
   width: calc(100vw - 162px) !important;
   max-width: calc(100vw - 162px) !important;
-  grid-template-columns: 100px 100px 100px repeat(8, minmax(90px, 1fr)) 120px repeat(
-      2,
-      minmax(90px, 1fr)
-    );
+  grid-template-columns: 100px 100px 100px repeat(8, minmax(90px, 1fr)) 120px repeat(2, minmax(90px, 1fr));
 }
 .management {
   position: relative;
