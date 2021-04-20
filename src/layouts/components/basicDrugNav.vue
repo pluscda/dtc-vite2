a489c5913531243673ba07d2581a695c28afecfe
alex <pluscda@gmail.com>
2021-04-20 09:01:44 +0800

    g

--- src/layouts/components/basicDrugNav.vue
+++ src/layouts/components/basicDrugNav.vue
@@ -1,7 +1,14 @@
 <template>
   <h2 class="pt-3 text-gray-400 font-thin text-base">藥局 / 基本資料設定</h2>
-  <div v-for="(item, i) in titles" :key="item" class="cursor-pointer py-3" :class="activeTab == i ? 'active-tab' : ''" @click="handleChangeTab(item, i)">
-    {{ item }}
+  <div
+    v-for="(item, i) in titles"
+    :key="item"
+    class="cursor-pointer py-3 flex space-x-3 pl-2"
+    :class="activeTab == i ? 'active-tab' : ''"
+    @click="handleChangeTab(item, i)"
+  >
+    <i :class="icons[i]" class="inline-block mt-0.5"></i>
+    <p class="text-left">{{ item }}</p>
   </div>
   <!-- <DrugAdd /> -->
 </template>
@@ -21,6 +28,7 @@ export default {
     const activeTab = ref(0);
     const router = useRouter();
     const titles = reactive(["藥品資料維護", "新增藥品資料", "藥品廠商維護", "新增藥品廠商", "藥房資料維護", "新增藥房資料"]);
+    const icons = reactive(["el-icon-edit", "el-icon-circle-plus", "el-icon-s-tools", "el-icon-folder-add", "el-icon-pie-chart", "el-icon-plus"]);
     //option
 
     //global
@@ -54,6 +62,7 @@ export default {
       titles,
       handleChangeTab,
       activeTab,
+      icons,
     };
   },
 };
