7c9e516f52770a24140e3a704e12553fe8628984
alex <pluscda@gmail.com>
2021-04-21 08:59:27 +0800

    g

--- src/components/set5List.vue
+++ src/components/set5List.vue
@@ -1,8 +1,53 @@
 <template>
-  <section class="dtc-text text-xl">set5</section>
+  <section style="width: 99vw">
+    <header class="ml-1 dtc-grid-header dtc-grid-header__divs dtc-template-columns">
+      <div>查閱清單</div>
+      <div v-for="(item, i) in headers" :key="i" @click="sort(headers, item)">
+        {{ item.name }}
+        <span v-show="item.sortDesc === null">
+          <i-typcn:arrow-unsorted></i-typcn:arrow-unsorted>
+        </span>
+        <span v-show="item.sortDesc === false">
+          <i-typcn:arrow-sorted-down></i-typcn:arrow-sorted-down>
+        </span>
+        <span v-show="item.sortDesc">
+          <i-typcn:arrow-sorted-up></i-typcn:arrow-sorted-up>
+        </span>
+      </div>
+    </header>
+    <main
+      class="ml-1 text-black cursor-pointer dtc-grid-header dtc-grid-body dtc-template-columns"
+      v-for="(item, i) in list"
+      :key="i"
+      :style="i % 2 == 0 ? 'background-color: #F5F5F5;' : 'background-color: #E0E0E0;'"
+    >
+      <div>
+        <InputSwitch class="transform translate-y-1.5" v-model="item.review" size="small" @click.stop="toggleDetail(item)"></InputSwitch>
+      </div>
+      <div>{{ i + 1 }}</div>
+      <div>{{ item.registerTimestamp.split("T")[0] }}</div>
+      <div>{{ item.registerSection || "暫無資料" }}</div>
+      <div>{{ item.registerNo || "暫無資料" }}</div>
+      <div>{{ item.registerName || "暫無資料" }}</div>
+      <div>{{ item.registerAlias || "暫無資料" }}</div>
+      <div>{{ item.category || "暫無資料" }}</div>
+      <div>{{ item.doctorNo || "暫無資料" }}</div>
+      <div>{{ item.doctorName || "暫無資料" }}</div>
+      <div>{{ item.totalRegisterNum || "暫無資料" }}</div>
+      <div>{{ item.totalCheckedNum || "暫無資料" }}</div>
+    </main>
+
+    <!-- 分頁 -->
+    <!-- <pagination v-show="total > 0" :total="total" v-model:page="listQuery.page" v-model:limit="listQuery.limit" @pagination="getList"></pagination> -->
+  </section>
 </template>
 
 <script>
+import { toRefs, ref } from "vue";
+import { useRouter } from "vue-router";
+import { ElMessage } from "element-plus";
+import Pagination from "cps/Pagination.vue";
+import queryString from "qs";
 import { useList } from "/@/hooks/useHis.js";
 let headers = [
   { name: "藥品名稱", key: "registerTimestamp", sortDesc: null },
@@ -21,12 +66,60 @@ export default {
   data() {
     return {};
   },
-  components: {},
+  components: {
+    Pagination,
+  },
+  setup() {
+    const router = useRouter();
+    headers = ref(headers);
+    const { state, getList, sort, clearFilters } = useList("his-histories");
+
+    function handleEdit({ row }) {
+      router.push({
+        name: "userEdit",
+        params: { id: row.id },
+      });
+    }
+
+    function handleDelete({ row }) {
+      // delItem(row.id).then(() => {
+      //   Message.success("刪除成功！");
+      // });
+    }
+
+    const toggleDetail = (item) => {
+      const review = item.review;
+      state.list.forEach((s) => (s.review = false));
+      item.review = !review;
+    };
+
+    return {
+      ...toRefs(state),
+      getList,
+      handleEdit,
+      handleDelete,
+      headers,
+      toggleDetail,
+      sort,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .dtc-tabs::after {
   all: unset;
+}
+.dtc-template-columns {
+  grid-template-columns: repeat(11, minmax(90px, 120px)) minmax(90px, 1fr);
+}
+
+#app .dtc-grid-header__divs {
+  > div {
+    height: 34px;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+    line-height: 30px;
+  }
 }
 </style>
