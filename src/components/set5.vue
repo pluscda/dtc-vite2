ce23587673e56bf49959b4426bdd4fdcc2974101
alex <pluscda@gmail.com>
2021-04-21 08:17:32 +0800

    g

--- src/components/set5.vue
+++ src/components/set5.vue
@@ -12,6 +12,19 @@
 
 <script>
 import InqueryList from "/@/views/users/components/inqueryList.vue";
+let headers = [
+  { name: "藥品名稱", key: "registerTimestamp", sortDesc: null },
+  { name: "藥品類型", key: "registerSection", sortDesc: null },
+  { name: "給藥方式", key: "registerNo", sortDesc: null },
+  { name: "煎藥方式", key: "registerName", sortDesc: null },
+  { name: "單一用量", key: "registerAlias", sortDesc: null },
+  { name: "單位名稱", key: "category", sortDesc: null },
+  { name: "頻率代碼", key: "doctorNo", sortDesc: null },
+  { name: "頻率名稱", key: "doctorName", sortDesc: null },
+  { name: "天數", key: "totalRegisterNum", sortDesc: null },
+  { name: "總量", key: "totalCheckedNum", sortDesc: null },
+  { name: "待看診數", key: "totalWaitNum", sortDesc: null },
+];
 export default {
   data() {
     return {};
---
+++ src/hooks/useHis.js
@@ -0,0 +1,122 @@
+import { reactive, onMounted, ref } from "vue";
+import axios from "utils/request";
+import queryString from "qs";
+import { forkJoin, of } from 'rxjs';
+import { catchError } from "rxjs/operators";
+import { ElMessage } from "element-plus";
+
+const init =  { 
+  loading: true, 
+  list: [], 
+  total: 0,
+  listQuery: {
+    page: 1,
+    limit: 10,
+    sort:[],
+    filter:''
+  }
+};
+export function useList(url) {
+  const state = reactive({
+         ...init
+  });
+  function twTime(iso) {
+      let s = dayjs(iso).format("YYYY-MM-DDT00:00:00.000Z");
+      return s.split("T")[0];
+  }
+  function getList() {
+    if(!url) return;
+    state.list = [];
+    state.loading = true;
+    const {limit, page, sort} =  state.listQuery;
+    let queryObj = {
+      _limit : limit,
+      _start: page > 1 ? page * limit : 0,
+    }
+    sort.length ? queryObj._sort = sort.join(",") : '';
+    let qs = queryString.stringify(queryObj) + "&" + state.listQuery.filter;
+   
+    forkJoin(
+      {
+        total: axios.get(`${url}/count?` + qs ),
+        data: axios.get(`${url}?` + qs)
+      }
+    ).pipe( catchError( error => {
+      ElMessage.error("AJAX ${url} get list fail!!");
+      of({total:0, data:[]})
+    })).subscribe( ({total, data}) => {
+       state.total = total;
+       state.list = data;
+       state.loading = false;
+    })
+  }
+  function clearFilters(){
+    state.listQuery.filter = "";
+    getList();
+  }
+  async function getItemDetail(item){
+    try{
+      return await axios.get(`${url}/${item.id}`);
+    }catch(e){
+       ElMessage.error("AJAX ${url} get item detail faill!!");
+    }
+  }
+  async function removeItem(item){
+    try{
+      await axios.delete(`${url}/${item.id}`);
+      getList();
+    }catch(e){
+       ElMessage.error("AJAX ${url} delete item faill!!");
+    }
+  }
+
+  function sort(headers,item) {
+      if (item.sortDesc) {
+        item.sortDesc = null;
+      } else if (false === item.sortDesc) {
+        item.sortDesc = true;
+      } else if (null === item.sortDesc) {
+        item.sortDesc = false;
+      }
+      const orderBy = [];
+      headers.forEach((s) => {
+        if (s.sortDesc !== null) {
+          //ref: https://strapi.io/documentation/developer-docs/latest/developer-resources/content-api/content-api.html#sort
+          orderBy.push(s.sortDesc ? `${s.key}:desc` : `${s.key}:asc`);
+        }
+      });
+      state.listQuery.sort = orderBy;
+      getList();
+  }
+  // 首次獲取數據
+  getList();
+
+  return { state, getList, sort, clearFilters,removeItem,getItemDetail,twTime};
+}
+
+const defaultData = {
+  name: "",
+  age: undefined,
+};
+
+export function useItem(isEdit, id) {
+  const model = ref(Object.assign({}, defaultData));
+
+  const updateUser = () => {
+    return axios({
+      url: "/updateUser",
+      method: "post",
+      data: model.value,
+    });
+  };
+
+  const addUser = () => {
+    return axios({
+      url: "/addUser",
+      method: "post",
+      data: model.value,
+    });
+  };
+
+  return { model, updateUser, addUser };
+}
