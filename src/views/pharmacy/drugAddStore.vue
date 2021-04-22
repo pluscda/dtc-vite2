97d06441ac7d265074e602b822af5395b0601110
alex <pluscda@gmail.com>
2021-04-22 15:53:31 +0800

    g

--- src/views/pharmacy/drugAddStore.vue
+++ src/views/pharmacy/drugAddStore.vue
@@ -30,7 +30,7 @@
 import { ref, inject } from "vue";
 import { ElMessage } from "element-plus";
 import { forkJoin, of, Subject } from "rxjs";
-import { catchError, exhaustMap, takeUntil, throttleTime } from "rxjs/operators";
+import { exhaustMap, throttleTime } from "rxjs/operators";
 
 let subscribe = "";
 export default {
