ff5263aa7704b7c2d3b2fc43ec3b227b66fbf1f4
alex <pluscda@gmail.com>
2021-04-26 11:11:53 +0800

    g

--- src/components/set5.vue
+++ src/components/set5.vue
@@ -1,7 +1,7 @@
 <template>
   <section style="background: #4b5563; grid-column: 1/-1" class="dtc-door-way-section grid">
     <header class="flex gap-4 dtc-text pl-1 dtc-tabs" style="grid-column: 1/-1">
-      <div>用藥</div>
+      <div style="background: none !important" class="dtc-text cursor-auto">M 用藥</div>
       <div>組套</div>
       <div>設為組套</div>
     </header>
