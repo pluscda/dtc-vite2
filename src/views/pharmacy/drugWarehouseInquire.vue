bd18309d46de8e4824e3f04ed6880f482f568ac3
alex <pluscda@gmail.com>
2021-04-28 16:02:56 +0800

    g

--- src/views/pharmacy/drugWarehouseInquire.vue
+++ src/views/pharmacy/drugWarehouseInquire.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="management">
-    <header class="dtc-page-header grid dtc-page-header__grid pr-2">
+    <header class="dtc-page-header grid dtc-page-header__grid pr-2 my-long-header">
       <div>庫存查詢作業</div>
     </header>
     <nav class="ml-1 dtc-search-filters mt-4" style="margin-bottom: 1.5rem !important">
