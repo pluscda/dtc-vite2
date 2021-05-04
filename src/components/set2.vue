1a441e3753d76993776f1665c8a22a21bdb4e291
alex <pluscda@gmail.com>
2021-05-05 07:52:51 +0800

    g

--- src/components/set2.vue
+++ src/components/set2.vue
@@ -1,13 +1,13 @@
 <template>
   <section style="background: #4b5563" class="dtc-door-way-section">
     <header class="flex gap-4 dtc-text pl-0s dtc-tabs" style="">
-      <section style="background: none !important" class="dtc-text cursor-auto">O 客訴</section>
+      <section style="background: none !important" class="dtc-text cursor-auto">O 客觀檢查</section>
       <div>組套</div>
       <div>設為組套</div>
       <div>清除</div>
     </header>
     <nav class="w-full border-b-4" style="border-color: #646b74 !important"></nav>
-    <Textarea class="w-full" style="height: 250px" spellcheck="false" placeholder="請請輸入客訴..."></Textarea>
+    <Textarea class="w-full" style="height: 250px" spellcheck="false" placeholder="請請輸入客觀檢查..."></Textarea>
   </section>
 </template>
 
