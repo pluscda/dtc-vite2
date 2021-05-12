0d9cd66425b8ec2969e85506c60e926905c193fc
Poseidoncode <posidomhu@gmail.com>
2021-05-12 10:24:35 +0800

    new-xd-btn

--- src/layouts/components/basicDrugNav.vue
+++ src/layouts/components/basicDrugNav.vue
@@ -1,8 +1,32 @@
 <template>
   <h2 class="pt-3 text-base font-thin text-gray-400">
-    <span @click="open('https://xd.adobe.com/view/29fb97b8-9810-491b-b03f-96951dd35f75-7ca8/')">藥</span>
-    <span @click="open('https://xd.adobe.com/view/acb6b4a0-7b02-4ff9-9975-48e18266ae47-f9e5/')">局</span>
-    / 基本資料設定
+    <span
+      @click="
+        open(
+          'https://xd.adobe.com/view/29fb97b8-9810-491b-b03f-96951dd35f75-7ca8/'
+        )
+      "
+      >藥</span
+    >
+    <!-- 門診藥局設計圖 -->
+    <span
+      @click="
+        open(
+          'https://xd.adobe.com/view/acb6b4a0-7b02-4ff9-9975-48e18266ae47-f9e5/'
+        )
+      "
+      >局</span
+    >
+    /
+    <!-- 醫院藥局設計圖 -->
+    <span
+      @click="
+        open(
+          'https://xd.adobe.com/view/4905a95e-4e64-4311-9351-8e8eff2225a6-1c56/'
+        )
+      "
+      >基</span
+    >本資料設定
   </h2>
   <div
     v-for="(item, i) in titles"
@@ -33,8 +57,22 @@ export default {
   setup() {
     const activeTab = ref(0);
     const router = useRouter();
-    const titles = reactive(["藥品資料維護", "新增藥品資料", "藥品廠商維護", "新增藥品廠商", "藥房資料維護", "新增藥房資料"]);
-    const icons = reactive(["el-icon-edit", "el-icon-circle-plus-outline", "el-icon-s-tools", "el-icon-folder-add", "el-icon-pie-chart", "el-icon-plus"]);
+    const titles = reactive([
+      "藥品資料維護",
+      "新增藥品資料",
+      "藥品廠商維護",
+      "新增藥品廠商",
+      "藥房資料維護",
+      "新增藥房資料",
+    ]);
+    const icons = reactive([
+      "el-icon-edit",
+      "el-icon-circle-plus-outline",
+      "el-icon-s-tools",
+      "el-icon-folder-add",
+      "el-icon-pie-chart",
+      "el-icon-plus",
+    ]);
     const global = inject("global");
     pharmacyTab$.subscribe((v) => (activeTab.value = v));
     const handleChangeTab = (item, i) => {
