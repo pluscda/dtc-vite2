51a321df4b95b068fcebcb1130e13c33ceac4e1b
alex <pluscda@gmail.com>
2021-05-13 14:46:09 +0800

    g

--- src/utils/ajax.js
+++
@@ -1,9 +0,0 @@
-import { ajax } from 'rxjs/ajax';
-
-const defaultHeaders = {
-  Authorization: "Bearer " + sessionStorage.token,
-  'Content-Type': 'application/json',
-};
-export const get = (url) =>
-  ajax.get(import.meta.env.VITE_BASE_API + "/" + url, Object.assign({}, defaultHeaders));
-
