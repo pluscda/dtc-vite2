46193d835c92c7418528b79713ca68c5f814109b
alex <pluscda@gmail.com>
2021-06-09 10:05:08 +0800

    g

--- src/views/users/components/register.vue
+++ src/views/users/components/register.vue
@@ -8,7 +8,7 @@
     </header>
     <main class="grid gap-2 grid-cols-2" style="grid-template-rows: 40px">
       <DtxInputGroup prepend="掛號日期">
-        <Calendar class="h-10 w-full" v-model="regTime" placeholder="請輸入日期" :showIcon="true" dateFormat="yy-mm-dd" />
+        <Calendar @change="getOptDepartment" class="h-10 w-full" v-model="regTime" placeholder="請輸入日期" :showIcon="true" dateFormat="yy-mm-dd" />
       </DtxInputGroup>
       <DtxInputGroup prepend="看診科別">
         <el-select filterable v-model="dept" placeholder="請選擇" class="border-l-0" @change="getOptSpecialty">
@@ -66,15 +66,18 @@ export default {
         alert(e);
       }
     },
+    async getOptDepartment() {
+      try {
+        const time = this.regTime + this.global.zeros;
+        const { entry } = await this.actions.getOptDepartmentByDate(time);
+        this.deptt = entry;
+      } catch (e) {
+        alert("getOptDepartmentByDate: " + e);
+      }
+    },
   },
   async mounted() {
-    try {
-      const time = this.regTime + this.global.zeros;
-      const { entry } = await this.actions.getOptDepartmentByDate(time);
-      this.deptt = entry;
-    } catch (e) {
-      alert("getOptDepartmentByDate: " + e);
-    }
+    this.getOptDepartment();
   },
 };
 </script>
